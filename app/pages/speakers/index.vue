c7dbe7d4d43ea6aea8c95fe60e9fa72585d56d8d
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2025-03-23 16:51:41 +0100

    feat: add sideover with speaker details

--- app/pages/speakers/index.vue
+++ app/pages/speakers/index.vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
-import { useAsyncData } from '#app'
+import type { SpeakersCollectionItem } from '@nuxt/content';
 
-const { data: speakers } = await useAsyncData('speakers', () => queryCollection('speakers').all())
+const { data: speakers } = await useAsyncData('speakers', () => queryCollection('speakers').all());
+
+const selectedSpeaker = ref<SpeakersCollectionItem | null>(null);
+const openSpeakerDetails = (speaker: SpeakersCollectionItem) => {
+  selectedSpeaker.value = speaker;
+};
 </script>
 
 <template>
   <UContainer>
-    <UPageHeader
-      title="Speakers"
-      description="Meet our amazing speakers."
-      class="py-[50px]"
-    />
+    <UPageHeader title="Speakers" description="Meet our amazing speakers." class="py-[50px]" />
 
     <UPageBody>
-      <UPageGrid>
-        <SpeakerCard
-          v-for="speaker in speakers"
-          :key="speaker.firstname + speaker.lastname"
-          :speaker="speaker"
-        />
-      </UPageGrid>
+      <USlideover title="Speaker details">
+
+        <UPageGrid>
+          <SpeakerCard v-for="speaker in speakers" :key="speaker.firstname + speaker.lastname" :speaker="speaker"
+            @click="openSpeakerDetails(speaker)" />
+        </UPageGrid>
+
+        <template #body v-if="selectedSpeaker">
+          <div class="flex flex-col">
+            <UUser 
+              :name="`${selectedSpeaker.firstname} ${selectedSpeaker.lastname}`"
+              :description="selectedSpeaker.role"
+              :avatar="{
+                src: selectedSpeaker.photo
+              }"
+              size="xl" 
+              class="justify-center mb-10"
+              :ui="{avatar: 'size-24'}" />
+            <div v-if="selectedSpeaker.company" class="flex items-center space-x-2">
+              <UIcon name="lucide:building-2" class="text-gray-500" />
+              <p>{{ selectedSpeaker.company.name }}</p>
+            </div>
+            <USeparator class="mt-10"/>
+            <div v-if="selectedSpeaker.socials" class="mt-5 flex justify-center">
+              <SocialLinks :socials="selectedSpeaker.socials"/>
+            </div>
+          </div>
+        </template>
+      </USlideover>
     </UPageBody>
   </UContainer>
 </template>
