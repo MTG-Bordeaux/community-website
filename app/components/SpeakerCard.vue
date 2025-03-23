77449ef0e4ece8e10218844fbbadcb52681a17d9
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2025-03-23 03:06:08 +0100

    feat: Add Speakers page, with a SpakerCard component

---
+++ app/components/SpeakerCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { SpeakersCollectionItem } from '@nuxt/content';
+import SocialLinks from './SocialLinks.vue';
+
+defineProps<{ speaker: SpeakersCollectionItem }>();
+
+</script>
+
+<template>
+  <UPageCard spotlight spotlight-color="primary">
+    <div class="flex">
+        <div class="w-1/3 flex items-center justify-center">
+          <UAvatar
+            :src="speaker.photo"
+            :alt="`${speaker.firstname} ${speaker.lastname}`"
+            size="3xl"
+            class="size-24 text-4xl"
+          />
+        </div>
+        <div class="w-2/3 p-4 flex flex-col justify-center">
+          <h3 class="text-lg font-semibold">{{ speaker.firstname }} {{ speaker.lastname }}</h3>
+          <p class="text-sm text-gray-600">{{ speaker.role }}</p>
+          <p v-if="speaker.company" class="text-sm text-gray-500">
+            {{ speaker.company.name }}
+          </p>
+          <div v-if="speaker.socials" class="mt-2 flex">
+            <SocialLinks :socials="speaker.socials" />
+          </div>
+        </div>
+    </div>
+  </UPageCard>
+</template>
---
+++ app/pages/speakers/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useAsyncData } from '#app'
+
+const { data: speakers } = await useAsyncData('speakers', () => queryCollection('speakers').all())
+</script>
+
+<template>
+  <UContainer>
+    <UPageHeader
+      title="Speakers"
+      description="Meet our amazing speakers."
+      class="py-[50px]"
+    />
+
+    <UPageBody>
+      <UPageGrid>
+        <SpeakerCard
+          v-for="speaker in speakers"
+          :key="speaker.firstname + speaker.lastname"
+          :speaker="speaker"
+        />
+      </UPageGrid>
+    </UPageBody>
+  </UContainer>
+</template>
